d9e25f779b1c8f974bdfe26d07ea9693327aed03
Reinan Marques <wesleremarques@gmail.com>
2023-01-04 20:54:13 -0300

    tests in services

---
+++ backend/src/test/java/br/com/reinan/dscatalog/services/ProductServicesTests.java
@@ -0,0 +1,9 @@
+package br.com.reinan.dscatalog.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServicesTests {
+
+}
