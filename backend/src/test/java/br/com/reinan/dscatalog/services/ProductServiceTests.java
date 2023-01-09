d95abda3fe2edf252be58ceaa558bc9cbfef0f4d
Reinan Marques <wesleremarques@gmail.com>
2023-01-08 23:22:39 -0300

    tests update product Service

--- backend/src/test/java/br/com/reinan/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/br/com/reinan/dscatalog/services/ProductServiceTests.java
@@ -1,5 +1,6 @@
 package br.com.reinan.dscatalog.services;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 
@@ -10,12 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.com.reinan.dscatalog.dto.ProductDto;
+import br.com.reinan.dscatalog.entities.Product;
 import br.com.reinan.dscatalog.repositories.CategoryRepository;
 import br.com.reinan.dscatalog.repositories.ProductRepository;
 import br.com.reinan.dscatalog.services.exceptions.ResorceNotFoundException;
+import br.com.reinan.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -31,12 +34,17 @@ public class ProductServiceTests {
 
     private Long existingId;
     private Long notExistingId;
+    private Product product;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         notExistingId = 1000L;
+        product = Factory.createProduct();
 
+        Mockito.when(repository.getOne(existingId)).thenReturn(product);
+        Mockito.when(repository.getOne(notExistingId)).thenThrow(ResorceNotFoundException.class);
+        Mockito.when(repository.save(any())).thenReturn(product);
         Mockito.doNothing().when(repository).deleteById(existingId);
         Mockito.doThrow(ResorceNotFoundException.class).when(repository).deleteById(notExistingId);
     }
@@ -58,6 +66,22 @@ public class ProductServiceTests {
         });
 
         Mockito.verify(repository, times(1)).deleteById(notExistingId);
+    }
+
+    @Test
+    public void updateShouldReturnEntityUpadate() {
+        ProductDto entity = service.update(existingId, Factory.createProductDto());
+
+        Assertions.assertEquals(product.getName(), entity.getName());
+
+        Mockito.verify(repository, times(1)).getOne(existingId);
+    }
+
+    @Test
+    public void updateShouldThrowsResorceNotFoundExceptionWhenNotExistsId() {
+        Assertions.assertThrows(ResorceNotFoundException.class, () -> {
+            service.update(notExistingId, new ProductDto());
+        });
     }
 
 }
