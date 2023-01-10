e61dbff3e94bd95ba021b1f14fbf805854fe321c
Reinan Marques <wesleremarques@gmail.com>
2023-01-09 23:18:49 -0300

    Find All service

--- backend/src/test/java/br/com/reinan/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/br/com/reinan/dscatalog/services/ProductServiceTests.java
@@ -4,6 +4,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.reinan.dscatalog.dto.ProductDto;
@@ -35,18 +40,24 @@ public class ProductServiceTests {
     private Long existingId;
     private Long notExistingId;
     private Product product;
+    private PageImpl<Product> page;
 
+    @SuppressWarnings("deprecation")
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         notExistingId = 1000L;
         product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
 
         Mockito.when(repository.getOne(existingId)).thenReturn(product);
         Mockito.when(repository.getOne(notExistingId)).thenThrow(ResorceNotFoundException.class);
+        Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+        Mockito.when(repository.findById(notExistingId)).thenReturn(Optional.empty());
         Mockito.when(repository.save(any())).thenReturn(product);
         Mockito.doNothing().when(repository).deleteById(existingId);
         Mockito.doThrow(ResorceNotFoundException.class).when(repository).deleteById(notExistingId);
+        Mockito.when(repository.findAll((Pageable) any())).thenReturn(page);
     }
 
     @Test
@@ -68,12 +79,12 @@ public class ProductServiceTests {
         Mockito.verify(repository, times(1)).deleteById(notExistingId);
     }
 
+    @SuppressWarnings("deprecation")
     @Test
     public void updateShouldReturnEntityUpadate() {
         ProductDto entity = service.update(existingId, Factory.createProductDto());
 
-        Assertions.assertEquals(product.getName(), entity.getName());
-
+        Assertions.assertNotNull(entity);
         Mockito.verify(repository, times(1)).getOne(existingId);
     }
 
@@ -84,4 +95,21 @@ public class ProductServiceTests {
         });
     }
 
+    @Test
+    public void findByIdShouldReturnObjectNotNullWhenExistsId() {
+        ProductDto obj = service.findById(existingId);
+
+        Assertions.assertNotNull(obj);
+
+        Mockito.verify(repository).findById(existingId);
+    }
+
+    @Test
+    public void findByIdShouldThrowsResorceNotFoundExceptionWhenNotExistsId() {
+        Assertions.assertThrows(ResorceNotFoundException.class, () -> {
+            service.findById(notExistingId);
+        });
+
+        Mockito.verify(repository).findById(notExistingId);
+    }
 }
