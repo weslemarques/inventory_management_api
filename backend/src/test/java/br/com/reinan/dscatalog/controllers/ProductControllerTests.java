6c40ed048b4d18fe93f7c62e381aa9cd3381c9c1
Reinan Marques <wesleremarques@gmail.com>
2023-01-14 16:09:06 -0300

    adicionando tests findById and delete

--- backend/src/test/java/br/com/reinan/dscatalog/controllers/ProductControllerTests.java
+++ backend/src/test/java/br/com/reinan/dscatalog/controllers/ProductControllerTests.java
@@ -1,6 +1,8 @@
 package br.com.reinan.dscatalog.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -12,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
@@ -20,6 +21,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import br.com.reinan.dscatalog.dto.ProductDto;
 import br.com.reinan.dscatalog.services.ProductService;
+import br.com.reinan.dscatalog.services.exceptions.DataBaseException;
+import br.com.reinan.dscatalog.services.exceptions.ResorceNotFoundException;
 import br.com.reinan.dscatalog.tests.Factory;
 
 @WebMvcTest(ProductController.class)
@@ -32,14 +35,27 @@ public class ProductControllerTests {
 
     private ProductDto productDto;
     private PageImpl<ProductDto> page;
+    private Long existId;
+    private Long notExistId;
+    private Long dependenceId;
 
     @BeforeEach
     void setUpI() throws Exception {
+
+        existId = 1L;
+        notExistId = 2L;
+        dependenceId = 3L;
 
         productDto = Factory.createProductDto();
         page = new PageImpl<>(List.of(productDto));
 
         when(service.findAll(any())).thenReturn(page);
+        when(service.findById(existId)).thenReturn(productDto);
+        doThrow(ResorceNotFoundException.class).when(service).findById(notExistId);
+        doNothing().when(service).delete(existId);
+        doThrow(ResorceNotFoundException.class).when(service).delete(notExistId);
+        doThrow(DataBaseException.class).when(service).delete(dependenceId);
+
     }
 
     @Test
@@ -50,6 +66,32 @@ public class ProductControllerTests {
                 .andExpect(status().isOk());
 
         verify(service).findAll(any(Pageable.class));
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+        mvc.perform(get("/products/{id}", existId))
+                .andExpect(status().isOk());
+
+        verify(service).findById(existId);
+    }
+
+    @Test
+    public void testFindByIdThrowsResorceNotFoundException() throws Exception {
+        mvc.perform(get("/products/{id}", notExistId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteVoid() throws Exception {
+        mvc.perform(delete("/products/{id}", existId))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testDeleteThrowsResorceNotFoundException() throws Exception {
+        mvc.perform(delete("/products/{id}", notExistId))
+                .andExpect(status().isNotFound());
     }
 
 }
