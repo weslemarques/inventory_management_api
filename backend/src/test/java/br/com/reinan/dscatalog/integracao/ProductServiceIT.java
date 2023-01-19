3b7ca21f1a8647c325c1e4e743d2232979a5498f
Reinan Marques <wesleremarques@gmail.com>
2023-01-19 20:41:38 -0300

    entendendo as anotations do IT

--- backend/src/test/java/br/com/reinan/dscatalog/integracao/ProductServiceIT.java
+++ backend/src/test/java/br/com/reinan/dscatalog/integracao/ProductServiceIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +17,7 @@ import br.com.reinan.dscatalog.services.ProductService;
 import br.com.reinan.dscatalog.services.exceptions.ResorceNotFoundException;
 import br.com.reinan.dscatalog.tests.Factory;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Transactional
 public class ProductServiceIT {
 
