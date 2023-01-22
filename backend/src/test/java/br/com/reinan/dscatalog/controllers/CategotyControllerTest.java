dfe61d533237f55ac1b6cf7a04794748aef4f082
Reinan Marques <wesleremarques@gmail.com>
2023-01-21 23:33:08 -0300

    test delete repositoryCategory

---
+++ backend/src/test/java/br/com/reinan/dscatalog/controllers/CategotyControllerTest.java
@@ -0,0 +1,5 @@
+package br.com.reinan.dscatalog.controllers;
+
+public class CategotyControllerTest {
+
+}
---
+++ backend/src/test/java/br/com/reinan/dscatalog/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,44 @@
+package br.com.reinan.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import br.com.reinan.dscatalog.entities.Category;
+
+@DataJpaTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    private Long existsId;
+    private Long notExistsId;
+
+    @BeforeEach
+    public void setUp() {
+        existsId = 1L;
+        notExistsId = 1000L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdexist() {
+        repository.deleteById(existsId);
+
+        Optional<Category> obj = repository.findById(existsId);
+
+        Assertions.assertTrue(obj.isEmpty());
+    }
+
+    @Test
+    public void deleteShouldThrowsResorceNotFoundExceptionWhenNotExistsId() {
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+            repository.deleteById(notExistsId);
+        });
+    }
+}
