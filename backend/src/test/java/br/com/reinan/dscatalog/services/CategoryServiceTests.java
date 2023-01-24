8a97d2b8bbc75da1a5b4e95a91de9d3f44c8b004
Reinan Marques <wesleremarques@gmail.com>
2023-01-24 17:49:10 -0300

    findById tests category services

--- backend/src/test/java/br/com/reinan/dscatalog/services/CategoryServiceTests.java
+++ backend/src/test/java/br/com/reinan/dscatalog/services/CategoryServiceTests.java
@@ -2,6 +2,9 @@ package br.com.reinan.dscatalog.services;
 
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +15,11 @@ import org.mockito.Mock;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.com.reinan.dscatalog.dto.CategoryDto;
+import br.com.reinan.dscatalog.entities.Category;
 import br.com.reinan.dscatalog.repositories.CategoryRepository;
 import br.com.reinan.dscatalog.services.exceptions.ResorceNotFoundException;
+import br.com.reinan.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class CategoryServiceTests {
@@ -26,13 +32,17 @@ public class CategoryServiceTests {
 
     private Long existsId;
     private Long notExistsId;
+    private Category category;
 
     @BeforeEach
     public void setUp() {
         existsId = 1L;
+        category = Factory.createCategory();
         notExistsId = 1000L;
         doNothing().when(repository).deleteById(existsId);
         doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(notExistsId);
+        when(repository.findById(existsId)).thenReturn(Optional.of(category));
+        when(repository.findById(notExistsId)).thenReturn(Optional.empty());
     }
 
     @Test
@@ -48,4 +58,20 @@ public class CategoryServiceTests {
             service.delete(notExistsId);
         });
     }
+
+    @Test
+    public void findByIdShouldReturnOptionalNotEmptyWhenIdExists() {
+        CategoryDto dto = service.findById(existsId);
+
+        Assertions.assertNotNull(dto);
+        Assertions.assertEquals("category", dto.getName());
+    }
+
+    @Test
+    public void findByIdShouldReturnOptionalEmptyWhenIdExists() {
+        Assertions.assertThrows(ResorceNotFoundException.class, () -> {
+            service.findById(notExistsId);
+        });
+    }
+
 }
