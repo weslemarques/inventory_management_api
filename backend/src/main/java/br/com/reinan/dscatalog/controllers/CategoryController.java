9f1e4c6b2ff676e8d21402b3e14d99524d2953d7
Reinan Marques <wesleremarques@gmail.com>
2023-02-11 22:58:05 -0300

    updates

--- backend/src/main/java/br/com/reinan/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/br/com/reinan/dscatalog/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.reinan.dscatalog.dto.CategoryDTO;
 import br.com.reinan.dscatalog.services.CategoryService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/categories")
 public class CategoryController {
 
--- backend/src/main/java/br/com/reinan/dscatalog/controllers/ProductController.java
+++ backend/src/main/java/br/com/reinan/dscatalog/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import br.com.reinan.dscatalog.services.ProductService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/products")
 public class ProductController {
 
