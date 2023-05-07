bab2d9dd82817d4a185499facfec3ada33ec5a44
reinanmarques <wesleremarques@gmail.com>
2023-05-07 20:58:04 -0300

    authorizacion with jwt

--- backend/src/main/java/br/com/reinan/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/br/com/reinan/dscatalog/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.reinan.dscatalog.config;
 
+import br.com.reinan.dscatalog.services.FilterToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,11 +10,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private FilterToken filter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,7 +30,8 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.POST,"/auth/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/users/**", "/products", "/categories").hasRole("ADMIN")
-                .and()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable();
         return http.build();
     }
