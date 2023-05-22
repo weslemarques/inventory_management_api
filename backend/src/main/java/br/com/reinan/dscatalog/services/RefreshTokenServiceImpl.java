fb76e9a333901462f30eb3aa88d3f0538647570f
reinanmarques <wesleremarques@gmail.com>
2023-05-22 16:49:58 -0300

    refreshOtkenService Implements

--- backend/src/main/java/br/com/reinan/dscatalog/services/RefreshTokenServiceImpl.java
+++ backend/src/main/java/br/com/reinan/dscatalog/services/RefreshTokenServiceImpl.java
@@ -1,8 +1,23 @@
 package br.com.reinan.dscatalog.services;
 
+import br.com.reinan.dscatalog.entities.RefreshToken;
 import br.com.reinan.dscatalog.services.contract.RefreshTokenService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RefreshTokenServiceImpl implements RefreshTokenService {
+    @Override
+    public String findByToken(String token) {
+        return null;
+    }
+
+    @Override
+    public String createRefreshToken(Long userId) {
+        return null;
+    }
+
+    @Override
+    public String verifyExpiration(RefreshToken refreshToken) {
+        return null;
+    }
 }
