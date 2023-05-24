8fcc357569778084bc0d9647b814c23e0432eacd
reinanmarques <wesleremarques@gmail.com>
2023-05-24 10:27:31 -0300

    readme

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -6,14 +6,12 @@ INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Mario', 'G
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
-INSERT INTO tb_role (authority) VALUES ('ROLE_USER');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (4, 3);
 
 
 INSERT INTO tb_category (name, created_At) VALUES ('Books', NOW());
