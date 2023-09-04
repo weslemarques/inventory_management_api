6ba56bdd112985d13ab1703f43eaaf82d83eb193
wesmarques <wesleremarques@gmail.com>
2023-09-04 17:10:19 -0300

    refactoring code

--- backend/src/main/java/br/com/reinan/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/br/com/reinan/dscatalog/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package br.com.reinan.dscatalog.config;
 
 import br.com.reinan.dscatalog.config.exceptionConfig.handler.UnauthorizedHandler;
 import br.com.reinan.dscatalog.security.filter.FilterToken;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,41 +14,18 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.logging.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     private FilterToken filterToken;
+    private Filter filter;
 
     public WebSecurityConfig(FilterToken filterToken) {
         this.filterToken = filterToken;
     }
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http.authorizeHttpRequests() // Autorização de requests.
-//                .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/h2").permitAll() // Permitindo os recursos do swagger. (Todos podem acessar).
-//                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-//                .and()
-//                .csrf().disable().cors().disable()
-//                .authorizeHttpRequests()
-//                .requestMatchers("/v1/auth/**", "/v1/refreshtoken").permitAll()
-//                .requestMatchers(HttpMethod.GET, "/v1/products/**", "/v1/categories/**").permitAll()
-//                .requestMatchers(HttpMethod.DELETE, "/v1/products").hasAnyRole("OPERATOR", "ADMIN")
-//                .requestMatchers(HttpMethod.POST, "/v1/products/**").hasAnyRole("OPERATOR")
-//                .requestMatchers("/v1/users", "/v1/users/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//                .and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .headers().frameOptions().sameOrigin()
-//                .and()
-//
-//                //.and().exceptionHandling().accessDeniedHandler();
-//        return http.build();
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
--- backend/src/main/java/br/com/reinan/dscatalog/security/filter/FilterToken.java
+++ backend/src/main/java/br/com/reinan/dscatalog/security/filter/FilterToken.java
@@ -9,7 +9,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
