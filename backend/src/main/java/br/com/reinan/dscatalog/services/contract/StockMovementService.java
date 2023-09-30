b1c3bc6f587245164f91ba10c35fea6745f4724b
wesmarques <wesleremarques@gmail.com>
2023-09-30 18:16:29 -0300

    StockMovementService

--- backend/src/main/java/br/com/reinan/dscatalog/services/contract/StockMovementService.java
+++ backend/src/main/java/br/com/reinan/dscatalog/services/contract/StockMovementService.java
@@ -1,5 +1,9 @@
 package br.com.reinan.dscatalog.services.contract;
 
+import java.util.List;
+
 public interface StockMovementService {
 
+    boolean productSale(List<Long> products);
+    void addStock(int amount, Long id);
 }
